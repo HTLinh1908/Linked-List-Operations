9a920fff3726b67d76ac75a27d41a5311c395dda
shin1501 <lg.kenny2004.sis.vungtau@gmail.com>
2025-05-02 14:17:58 +0700

    Update ListController.java

--- src/main/java/com/linkedlistoperations/controller/ListController.java
+++ src/main/java/com/linkedlistoperations/controller/ListController.java
@@ -2,25 +2,95 @@ package com.linkedlistoperations.controller;
 
 import com.linkedlistoperations.model.*;
 import com.linkedlistoperations.view.ListPanel;
+
 import java.util.List;
 
 public class ListController<T> {
     private final LinkedList<T> model;
     private ListPanel<T> view;
 
-    public ListController(LinkedList<T> model) { this.model = model; }
-    public void setView(ListPanel<T> view) { this.view = view; }
+    public ListController(LinkedList<T> model) {
+        this.model = model;
+        model.addListChangeListener(this::refreshView);
+    }
 
-    public void insertFront(T data) { }
-    public void insertEnd(T data) { }
-    public void insertAt(int index, T data) { }
+    public void setView(ListPanel<T> view) {
+        this.view = view;
+        refreshView(); // update GUI initially
+    }
 
-    public void removeAt(int index) { }
-    public void removeValue(T value) { }
-    public void reverse() { }
+    private void refreshView() {
+        if (view != null) {
+            List<Node<T>> nodeList = model.toList();
+            view.updateList(nodeList);
+            view.showMessage(""); // clear message
+        }
+    }
 
-    public void search(T value) { }
-    public void detectCycle() { }
-    public void findMiddle() { }
+    public void insertFront(T data) {
+        model.insertFront(data);
+    }
+
+    public void insertEnd(T data) {
+        model.insertEnd(data);
+    }
+
+    public void insertAt(int index, T data) {
+        model.insertAt(index, data);
+    }
+
+    public void removeAt(int index) {
+        T removed = model.removeAt(index);
+        if (view != null) {
+            if (removed != null) {
+                view.showMessage("Removed item at index " + index + ": " + removed);
+            } else {
+                view.showMessage("Invalid index: " + index);
+            }
+        }
+    }
+
+    public void removeValue(T value) {
+        boolean success = model.removeValue(value);
+        if (view != null) {
+            view.showMessage(success
+                    ? "Removed value: " + value
+                    : "Value not found: " + value);
+        }
+    }
+
+    public void reverse() {
+        model.reverse();
+        if (view != null) view.showMessage("List reversed.");
+    }
+
+    public void search(T value) {
+        List<Node<T>> found = model.search(value);
+        if (view != null) {
+            if (found.isEmpty()) {
+                view.showMessage("Value not found: " + value);
+            } else {
+                view.showMessage("Found " + found.size() + " occurrence(s) of: " + value);
+                view.highlightValue(value);
+            }
+        }
+    }
+
+    public void detectCycle() {
+        boolean hasCycle = model.detectCycle();
+        if (view != null) {
+            view.showMessage(hasCycle ? "Cycle detected!" : "No cycle detected.");
+        }
+    }
+
+    public void findMiddle() {
+        Node<T> mid = model.findMiddle();
+        if (view != null) {
+            if (mid != null) {
+                view.showMessage("Middle element: " + mid.getData());
+            } else {
+                view.showMessage("List is empty.");
+            }
+        }
+    }
 }
-
